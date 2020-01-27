ff6f512659230676d6614bc34abf90025b330c20
Manuel Fernandez <manuel.fernandez@sbsdomain.local>
2020-01-27 23:36:24 +0000

    Script that calculates BMI

---
+++ bmi.py
@@ -0,0 +1,14 @@
+# Script that calculates the Body Mass Index (BMI)
+# dividing the weight by the hight in metres square
+
+# Definition of the variables introduced by the user
+
+Weight = float(input("Enter weight (in Kg)"))
+Height = float(input("Enter Height (in cm)"))*0.01
+
+# BMI formula
+
+bmi = Weight/((Height*Height))
+
+#Print result
+print (round(bmi,2))
