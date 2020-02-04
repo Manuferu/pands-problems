f6af10151291ef60a19a9803e82ef759a8a70e5e
Manuel Fernandez <manuel.fernandez@sbsdomain.local>
2020-02-04 11:57:54 +0000

    program that takes asks a user to input a string and outputs every second letter in reverse order

---
+++ secondstring.py
@@ -0,0 +1,8 @@
+# Script that calculates the Body Mass Index (BMI)
+# dividing the weight by the hight in metres square
+
+# Definition of the variables introduced by the user
+
+s = input("Please enter a sentence:")
+
+print(s[len(s):0:-2])
