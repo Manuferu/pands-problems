dd6ab053d65f4ec9b25ab31e710d398b04739f80
Manuel Fernandez <manuel.fernandez@sbsdomain.local>
2020-04-02 22:03:50 +0100

    Week 4 excercice. Write a program that asks the user to input any positive integer and outputs the successive values of the following calculation (modified)

--- squareroot.py
+++ squareroot.py
@@ -1,10 +1,21 @@
-#Task week 6, program ask you for a float number and gives you back an aprox.
+#Manuel Fernandez
+#Task week 6, program ask you for a float number and gives you back an aprox. square root following newton's method
+# reference taken about Newton's method: (Calculus/Newton's Method (wikibooks), https://en.wikibooks.org/wiki/Calculus/Newton%27s_Method)
+
 
 # Introduce a number
 number =float(input("Please enter a positive number:"))
 #function that calculates square roots by powering the number to 0.5
 def sqrt(x):
-    return x**(0.5)
+   #define a function to calculate square root applying Newton's method
+    # variable n is the aproximate value, is teh result of the derivate of sqaure root
+    n=0.5*x 
+    # variable m represents the next aproximation value following Newton's method formula
+    m= 0.5*(n+(x/n))
+    while m != n: #iteration until the two variables get closer (in our case , we will make it equal until the first decimal place. If not keep iterating)
+        n=m
+        m= 0.5*(n+(x/n))
+    return n
 
 #in case the user introduces a negative number, the program will force to introduce
 #a positive one.
--- weekday.py
+++ weekday.py
@@ -6,11 +6,11 @@ now =datetime.datetime.now().weekday()
 
 L=[]
 #iterate from 0 to 6
-for i in range(0,7):
+for i in range(0,6):
    #Create a Lit with 7 entries where the first 5 are considered weekday and th last 2 weekends.
-    if i <5:
+    if i <5: #first 5 positions in the list are populated with the message associated to weekdays
         L.append("Yes, unfortunately today is a weekday.")
-    else:
+    else:# the other option, which will be the last two positions,has associated the weekend's message
         L.append("It is the weekend, yay!")
 #Print the entry in the list that corresponds with the number of the variable now above created
 print (L[now])
